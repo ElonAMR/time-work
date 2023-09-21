7f64067ec5785bb14c6f8a83834df6b0fd9dce4b
אלון עמרם <elonamrammm1234@gmail.com>
2023-09-21 16:53:45 +0300

    יצירת UPDATE TO NAME

--- routers/employeesR.js
+++ routers/employeesR.js
@@ -18,3 +18,18 @@ router.post("/Add",(req, res) => {
 });
 
 
+router.patch("/Edit/:row_id",(req, res) => {
+    let id=req.params.row_id;
+    let name=req.body.name;
+    let q=`UPDATE \`employees\`  SET \`name\`='${name}' WHERE id=${id} `;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+});
+
+
